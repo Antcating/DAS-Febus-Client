1a2d3bbda431a7fd856a9c625c5c67466bd0103d
Antcating <evgenij.fedorchenko@gmail.com>
2023-10-29 17:13:53 +0200

    feat: NASPATH deleted

--- src/config.py
+++ src/config.py
@@ -6,17 +6,17 @@ config.read("config.ini")
 
 # PATHs to files and save
 LOCALPATH = config["PATH"]["LOCALPATH"]
-NASPATH = config["PATH"]["NASPATH"]
 LOCALPATH_final = config["PATH"]["LOCALPATH_final"]
 NASPATH_final = config["PATH"]["NASPATH_final"]
 
 # Difference between PACKET and SAVE times
 TIME_DIFF_THRESHOLD = int(config["CONSTANTS"]["TIME_DIFF_THRESHOLD"])
 DATA_LOSE_THRESHOLD = int(config["CONSTANTS"]["DATA_LOSE_THRESHOLD"])
+# Concat time
+CONCAT_TIME = int(config["CONSTANTS"]["CONCAT_TIME"])
 
-if isdir(NASPATH):
-    PATH = NASPATH
-elif isdir(LOCALPATH):
+
+if isdir(LOCALPATH):
     PATH = LOCALPATH
 else: 
     raise Exception('PATH is not accessible!')
