95c81cf1d79db4ca27c9aa314afcc55c0b0672f9
Antcating <evgenij.fedorchenko@gmail.com>
2023-10-29 18:04:29 +0200

    refactor: removed attrs dependency

--- src/config.py
+++ src/config.py
@@ -14,7 +14,8 @@ TIME_DIFF_THRESHOLD = int(config["CONSTANTS"]["TIME_DIFF_THRESHOLD"])
 DATA_LOSE_THRESHOLD = int(config["CONSTANTS"]["DATA_LOSE_THRESHOLD"])
 # Concat time
 CONCAT_TIME = int(config["CONSTANTS"]["CONCAT_TIME"])
-
+# Packet length in seconds
+UNIT_SIZE = int(config["CONSTANTS"]["UNIT_SIZE"])
 
 if isdir(LOCALPATH):
     PATH = LOCALPATH
