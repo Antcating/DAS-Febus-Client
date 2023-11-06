028263c6c4cfbad6e15eb15b217197ec25fd76f0
Antcating <yevhenii.fedorchenko@gmail.com>
2023-11-06 18:18:12 +0200

    refactor: :recycle: Connected client to global conf

--- src/config.py
+++ src/config.py
@@ -4,36 +4,33 @@ from os.path import isdir
 config = configparser.ConfigParser()
 config.read("config.ini", encoding="UTF-8")
 
-# PATHs to files and save
-LOCALPATH = config["PATH"]["LOCALPATH"]
-LOCALPATH_final = config["PATH"]["LOCALPATH_final"]
-NASPATH_final = config["PATH"]["NASPATH_final"]
-
+#
+# CONCATENATION CHARACTERISTICS
+#
 # Difference between PACKET and SAVE times
 TIME_DIFF_THRESHOLD = int(config["CONSTANTS"]["TIME_DIFF_THRESHOLD"])
 DATA_LOSE_THRESHOLD = int(config["CONSTANTS"]["DATA_LOSE_THRESHOLD"])
 # Concatenated chunk size (in seconds)
 CHUNK_SIZE = int(config["CONSTANTS"]["CONCAT_TIME"])
-# Individual packet length (in seconds)
-UNIT_SIZE = int(config["CONSTANTS"]["UNIT_SIZE"])
 
+# PACKET CHARACTERISTICS
+UNIT_SIZE = int(config["CONSTANTS"]["UNIT_SIZE"])
 SPS = int(config["CONSTANTS"]["SPS"])
+DX = float(config["CONSTANTS"]["DX"])
 
 TIME_SAMPLES = SPS * UNIT_SIZE
 SPACE_SAMPLES = int(config["CONSTANTS"]["SPACE_SAMPLES"])
 
+#
+# CLIENT CHARACTERISTICS
+#
+IP = config["CLIENT"]["IP"]
+PORT = config["CLIENT"]["PORT"]
 
-# def get_dims(path_abs: str):
-#     if isfile(path_abs):
-#         with open(path_abs, "r") as attrs_json:
-#             attrs: dict = json.load(attrs_json)
-#             SPACE_SAMPLES = (attrs["index"][1] + 1) / attrs["down_factor_space"]
-#             TIME_SAMPLES = (attrs["index"][1] + 1) / attrs["down_factor_time"]
-#     else:
-#         TIME_SAMPLES = SPS * UNIT_SIZE
-#         SPACE_SAMPLES = SPS * UNIT_SIZE
-#     return SPACE_SAMPLES, TIME_SAMPLES
-
+# PATHs to files and save
+LOCALPATH = config["PATH"]["LOCALPATH"]
+LOCALPATH_final = config["PATH"]["LOCALPATH_final"]
+NASPATH_final = config["PATH"]["NASPATH_final"]
 
 if isdir(LOCALPATH):
     PATH = LOCALPATH
