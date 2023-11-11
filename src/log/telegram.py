128661fa24015c73147069488fe7c8ef52dbd136
Antcating <yevhenii.fedorchenko@gmail.com>
2023-11-11 17:58:20 +0200

    fix: :bug: Exceptions added

--- src/log/telegram.py
+++ src/log/telegram.py
@@ -4,11 +4,24 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini", encoding="UTF-8")
 
-token = config["Telegram"]["token"]
-error_channel = config["Telegram"]["channel"]
 
-bot = telebot.TeleBot(token=token, parse_mode="None")
+def get_bot():
+    token = config["Telegram"]["token"]
+    return telebot.TeleBot(token=token, parse_mode="None")
+
+
+def get_channel():
+    return config["Telegram"]["channel"]
 
 
 def send_telegram_error(message: str):
-    bot.send_message(error_channel, message)
+    try:
+        use_telegram = config["Telegram"]["USE_TELEGRAM"]
+
+        if use_telegram == "1":
+            bot = get_bot()
+            channel = get_channel()
+            bot.send_message(channel, message)
+    except KeyError as err:
+        print("Required by Telegram-bot field is not found: \n\n", err)
+        return
