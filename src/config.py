404193865e71f2b5cf0e3e18e6cc3c5faf98b094
Antcating <yevhenii.fedorchenko@gmail.com>
2023-11-14 12:35:51 +0200

    docs: :memo: Updated config

--- src/config.py
+++ src/config.py
@@ -1,41 +1,23 @@
 import configparser
 from os.path import isdir
 
-config = configparser.ConfigParser()
-config.read("config.ini", encoding="UTF-8")
-
-#
-# CONCATENATION CHARACTERISTICS
-#
-# Difference between PACKET and SAVE times
-TIME_DIFF_THRESHOLD = float(config["CONSTANTS"]["TIME_DIFF_THRESHOLD"])
-DATA_LOSE_THRESHOLD = int(config["CONSTANTS"]["DATA_LOSE_THRESHOLD"])
-# Concatenated chunk size (in seconds)
-CHUNK_SIZE = int(config["CONSTANTS"]["CONCAT_TIME"])
+config_dict = configparser.ConfigParser()
+config_dict.read("config.ini", encoding="UTF-8")
 
 # PACKET CHARACTERISTICS
-UNIT_SIZE = int(config["CONSTANTS"]["UNIT_SIZE"])
-SPS = int(config["CONSTANTS"]["SPS"])
-DX = float(config["CONSTANTS"]["DX"])
-
-TIME_SAMPLES = SPS * UNIT_SIZE
-SPACE_SAMPLES = int(config["CONSTANTS"]["SPACE_SAMPLES"])
+SPS = int(config_dict["CONSTANTS"]["SPS"])
+DX = float(config_dict["CONSTANTS"]["DX"])
 
 #
 # CLIENT CHARACTERISTICS
 #
-IP = config["CLIENT"]["IP"]
-PORT = config["CLIENT"]["PORT"]
+IP = config_dict["CLIENT"]["IP"]
+PORT = config_dict["CLIENT"]["PORT"]
 
 # PATHs to files and save
-LOCALPATH = config["PATH"]["LOCALPATH"]
-NASPATH_final = config["PATH"]["NASPATH_final"]
+LOCALPATH = config_dict["PATH"]["LOCALPATH"]
 
 if isdir(LOCALPATH):
     PATH = LOCALPATH
 else:
     raise Exception("PATH is not accessible!")
-if isdir(NASPATH_final):
-    SAVE_PATH = NASPATH_final
-else:
-    raise Exception("SAVE_PATH is not accessible!")
