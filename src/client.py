26e867812054b79a1ee7c5eaeb567086ddfaf34b
Antcating <yevhenii.fedorchenko@gmail.com>
2023-11-14 12:35:21 +0200

    fix: :loud_sound: Updated logging

--- src/client.py
+++ src/client.py
@@ -1,7 +1,9 @@
 from client.main import main
 from log.telegram import send_telegram_error
+from log.main_logger import logger as log
 
 try:
     main()
 except Exception as err:
+    log.exception(f"Unexpected error in client: {err}")
     send_telegram_error("Unexpected error in client: \n\n" + str(err))
